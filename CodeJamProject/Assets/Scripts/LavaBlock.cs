bde2779022a10fe4cbaf666070713b8b878a907c
TheNitpickyCloud <67553169+TheNitpickyCloud@users.noreply.github.com>
2021-01-14 16:59:59 +0530

    commit-1
    added prefabs for quick level design

---
+++ CodeJamProject/Assets/Scripts/LavaBlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaBlock : MonoBehaviour
+{
+    public GameObject endPanel;
+    public GameObject pauseMenu;
+    public GameObject pauseButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        //play lava effect
+        if(player.CompareTag("Player"))
+        {
+            Destroy(player.gameObject);
+            endPanel.SetActive(true);
+        }
+    }
+}
---
+++ CodeJamProject/Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    public void pauseButton() 
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+    public void Resume() 
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void Restart() 
+    {
+        pauseMenu.SetActive(false);
+        SceneManager.LoadScene("Level1");
+        Time.timeScale = 1f;
+;    }
+
+    public void QuitGame() 
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+    }
+
+}
